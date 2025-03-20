bdcb8cf3a2cff4950a3c44878134fa3336296943
Bruno Ravanhani <bruno.ravanhanif@gmail.com>
2025-03-20 11:01:41 -0400

    terraform format

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,8 +3,8 @@ provider "aws" {
 }
 
 resource "aws_iam_role" "lambda_role" {
- name   = "terraform_aws_lambda_role"
- assume_role_policy = <<EOF
+  name               = "terraform_aws_lambda_role"
+  assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
   "Statement": [
@@ -25,10 +25,10 @@ EOF
 
 resource "aws_iam_policy" "iam_policy_for_lambda" {
 
-  name         = "aws_iam_policy_for_terraform_aws_lambda_role"
-  path         = "/"
-  description  = "AWS IAM Policy for managing aws lambda role"
-  policy = <<EOF
+  name        = "aws_iam_policy_for_terraform_aws_lambda_role"
+  path        = "/"
+  description = "AWS IAM Policy for managing aws lambda role"
+  policy      = <<EOF
 {
   "Version": "2012-10-17",
   "Statement": [
@@ -49,45 +49,45 @@ EOF
 # Policy Attachment on the role.
 
 resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
-  role        = aws_iam_role.lambda_role.name
-  policy_arn  = aws_iam_policy.iam_policy_for_lambda.arn
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.iam_policy_for_lambda.arn
 }
 
 # Generates an archive from content, a file, or a directory of files.
 
 data "archive_file" "zip_the_code" {
- type        = "zip"
- source_dir  = "../src"
- output_path = "../lambda.zip"
+  type        = "zip"
+  source_dir  = "../src"
+  output_path = "../lambda.zip"
 }
 
 # Create a lambda function
 # In terraform ${path.module} is the current directory.
 resource "aws_lambda_function" "terraform_lambda_func" {
- filename                       = "../lambda.zip"
- function_name                  = "children-movies-lambda-source"
- role                           = aws_iam_role.lambda_role.arn
- handler                        = "index.handler"
- runtime                        = "nodejs18.x"
- timeout                        = "10"
- memory_size                    = "128"
- source_code_hash               = data.archive_file.zip_the_code.output_base64sha256
- depends_on                     = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
+  filename         = "../lambda.zip"
+  function_name    = "children-movies-lambda-source"
+  role             = aws_iam_role.lambda_role.arn
+  handler          = "index.handler"
+  runtime          = "nodejs18.x"
+  timeout          = "10"
+  memory_size      = "128"
+  source_code_hash = data.archive_file.zip_the_code.output_base64sha256
+  depends_on       = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
 }
 
 resource "aws_lambda_function_url" "function" {
-    function_name      = aws_lambda_function.terraform_lambda_func.function_name
-    authorization_type = "NONE"
+  function_name      = aws_lambda_function.terraform_lambda_func.function_name
+  authorization_type = "NONE"
 }
 
 output "teraform_aws_role_output" {
- value = aws_iam_role.lambda_role.name
+  value = aws_iam_role.lambda_role.name
 }
 
 output "teraform_aws_role_arn_output" {
- value = aws_iam_role.lambda_role.arn
+  value = aws_iam_role.lambda_role.arn
 }
 
 output "teraform_logging_arn_output" {
- value = aws_iam_policy.iam_policy_for_lambda.arn
+  value = aws_iam_policy.iam_policy_for_lambda.arn
 }
--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -8,9 +8,9 @@ terraform {
   required_version = "~> 1.2"
 
   backend "s3" {
-    bucket = "bruno-ravanhani-site-terraform"
-    key = "children-movies-lambda/terraform.tfstate"
-    region = "us-east-1"
+    bucket         = "bruno-ravanhani-site-terraform"
+    key            = "children-movies-lambda/terraform.tfstate"
+    region         = "us-east-1"
     dynamodb_table = "children-movies-lambda-terraform"
   }
 }
